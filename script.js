d83fda2c368c7194f5d8513c5a673064fd02963c
ton <wattanachai.pu@ku.th>
2025-02-02 23:29:55 +0700

    Add image slider and password keypad functionality with responsive design

---
+++ script.js
@@ -0,0 +1,54 @@
+alert(
+  "สวัสดีน้าบี๊เว็ปนี้เค้าตั้งใจทำให้บี๊เลยผิดพลาดตรงไหนขออภัยด้วย ถ้างงอะไรกดปุ่มด้านบนขวามือได้เลยนะ"
+);
+// add disable double tap zoom
+// disable help
+function closeHelp() {
+  document.getElementById("helpMessage").style.display = "none";
+}
+function showHelp() {
+  document.getElementById("helpMessage").style.display = "block";
+}
+function addDigit(digit) {
+  let header = document.getElementById("header");
+  const passwordInput = document.getElementById("passwordInput");
+  if (passwordInput.value.length < 6) {
+    passwordInput.value += digit;
+  }
+
+  checkPassword();
+}
+
+function clearPassword() {
+  const passwordInput = document.getElementById("passwordInput");
+  passwordInput.value = "";
+}
+
+function deleteLastDigit() {
+  const passwordInput = document.getElementById("passwordInput");
+  passwordInput.value = passwordInput.value.slice(0, -1);
+}
+// function checkPassword() {
+//   if (passwordInput.value == "1234") {
+//     window.location.href = "index2.html";
+//   }
+// }
+let passAttempt = 0;
+function submitPassword() {
+  const passwordInput = document.getElementById("passwordInput");
+  if (passwordInput.value == "240267") {
+    window.location.href = "index2.html";
+  } else {
+    passAttempt++;
+  }
+  if (passwordInput.value == "240267") {
+    alert("ถูกต้อง");
+  } else if (passAttempt === 1) {
+    alert("ไม่ใช่รหัสนี้นะบ่าดี๊ 😭😭😭");
+  } else if (passAttempt === 2) {
+    alert("Incorrect Password3");
+  } else {
+    alert("รหัสก็คือวันครบรอบของเรางายยย");
+  }
+}
+document.querySelector(".submit-btn").addEventListener("click", submitPassword);
